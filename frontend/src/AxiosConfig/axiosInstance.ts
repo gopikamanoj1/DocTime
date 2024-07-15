a779ef60d51f7afc80fde32419ea710ee9d2aa8c
gopika <gopikamanoj001@gmail.com>
2024-07-15 19:12:36 +0530

    changes-maded

--- frontend/src/AxiosConfig/axiosInstance.ts
+++ frontend/src/AxiosConfig/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default settings
 const axiosInstance = axios.create({
-  baseURL: "https://doctime.live",
+  baseURL: "https://doctime.live/",
   timeout: 100000, // 10 seconds timeout
   headers: {
     'Content-Type': 'application/json',
